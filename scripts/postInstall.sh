6b092cde3919cdbbf0d5a7ba2c85664e65bffd1c
jbenguira <z51biz@gmail.com>
2022-11-27 10:37:45 +0200

    Fixes

---
+++ scripts/postInstall.sh
@@ -0,0 +1,12 @@
+#set env vars
+set -o allexport; source .env; set +o allexport;
+
+target=$(docker-compose port app 8090)
+
+curl http://${target}/api/admins \
+  -H 'accept: */*' \
+  -H 'accept-language: en-US' \
+  -H 'content-type: application/json' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36' \
+  --data-raw '{"email":"'$EMAIL'","password":"'$ADMIN_PASSWORD'","passwordConfirm":"'$ADMIN_PASSWORD'"}' \
+  --compressed
