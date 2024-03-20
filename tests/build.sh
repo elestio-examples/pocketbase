91a48d1fe900b3993a09a7da458b4e77ff945af1
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-03-20 17:27:50 +0200

    fix

--- tests/build.sh
+++ tests/build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
-api_url="https://api.github.com/repos/pocketbase/pocketbase/releases/latest"    
+api_url="https://api.github.com/repos/pocketbase/pocketbase/releases/latest"
 response=$(curl -s $api_url)
-latest_release_tag=$(echo $response | grep -o '"tag_name": *"[^"]*"' | awk -F'"' '{print $4}')
+latest_release_tag=$(echo $response | grep -o '"tag_name": *"[^"]*"' | awk -F'"' '{print $4}' | sed 's/^v//')
 
 sed -i "s~POCKETBASE_VERSION_TO_CHANGE~${latest_release_tag}~g" ./Dockerfile
 
