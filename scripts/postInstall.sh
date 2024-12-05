f91207b223a49af9a781026d0e8f38a9b6878b3b
AmsellemJoseph <amsellem.joseph@gmail.com>
2024-12-05 15:49:59 +0200

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -1,10 +1,17 @@
 #set env vars
 set -o allexport; source .env; set +o allexport;
 
+sleep 30s;
+docker-compose logs pocketbase > logs.txt
+token=$(grep -oP "http://0.0.0.0:8090/_/#/pbinstal/\K[^ ]+" logs.txt)
+
+rm -rf ./logs.txt
+
 target=$(docker-compose port pocketbase 8090)
 
-curl http://${target}/api/admins \
+curl http://${target}/api/collections/_superusers/records \
   -H 'accept: */*' \
+  -H 'authorization: '${token}'' \
   -H 'accept-language: en-US' \
   -H 'content-type: application/json' \
   -H 'user-agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36' \
